48982fabf4ac847e64decbd6ca70c729ae6397ec
murarivamsharaj <chmurari73@gmail.com>
2026-04-02 19:41:11 +0530

    feat: new class Super added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,12 @@
 public class HelloWorld{
     static void main() {
-        System.out.println("Hello World");
+        System.out.println("new program in java fundamentals");
     }
 }
 
 class Super{
     static void main() {
-        System.out.println("new class");
+        System.out.println("new   class");
 
     }
         }
