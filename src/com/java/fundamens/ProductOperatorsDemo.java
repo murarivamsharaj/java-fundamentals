d197e403b9211dcac0ff192d3ff3d7650815b1f8
murarivamsharaj <chmurari73@gmail.com>
2026-06-15 21:20:54 +0530

    feat: new changes added

--- src/com/java/fundamens/ProductOperatorsDemo.java
+++ src/com/java/fundamens/ProductOperatorsDemo.java
@@ -6,15 +6,18 @@ public class ProductOperatorsDemo {
         int quantity = 3;
         double discountPercent = 10;
         boolean isAvailable = true;
+        float maxDiscount = 100;
 
         double totalCost = price * quantity;
         double discountAmount = totalCost * discountPercent / 100;
         double finalPrice = totalCost - discountAmount;
         boolean canPurchase = isAvailable;
 
+
         System.out.println("Total Cost : " + totalCost);
         System.out.println("Discount:  " + discountAmount);
         System.out.println("Final Price: " + finalPrice);
         System.out.println("Can Purchase: " + canPurchase);
+        System.out.println("Total Cost: " + totalCost);
     }
 }
